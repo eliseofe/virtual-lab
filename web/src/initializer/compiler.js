351199dfa55202e510ba42a0a69f5763513e3670
Eliseo Ferrante <eliseofe@gmail.com>
2026-09-22 15:54:47 +0400

    #204 expose initializer symbol structure

--- web/src/initializer/compiler.js
+++ web/src/initializer/compiler.js
@@ -215,6 +215,19 @@ export function initializerStructure(source) {
   };
 }
 
+export function initializerStructure(source) {
+  const functions = parseProgram(source);
+  return {
+    language: "python-vlab-initializer/0.1",
+    symbols: [...functions.values()].map((fn) => ({
+      kind: "function",
+      name: fn.name,
+      line: fn.line,
+      params: [...fn.params],
+    })),
+  };
+}
+
 function pythonTruthy(value) { return Boolean(value); }
 
 function binary(op, a, b, line) {
